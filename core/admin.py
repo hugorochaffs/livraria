992d1db0596a3db9d0bca41ff2dbfaac263b7976
hugorochaffs <hugorochaffs@gmail.com>
2025-04-10 18:16:30 -0300

    deploy_heroku

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Categoria, Editora, Autor, Livro, Compra, ItensCompra
+
+admin.site.register(Categoria)
+admin.site.register(Editora)
+admin.site.register(Autor)
+admin.site.register(Livro)
+
+
+class ItensInline(admin.TabularInline):
+    model = ItensCompra
+
+
+@admin.register(Compra)
+class CompraAdmin(admin.ModelAdmin):
+    inlines = (ItensInline,)
